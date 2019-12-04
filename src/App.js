6fb47703bf10fd0187e32bd3560c4dee77b6dcfe
Chris Dalziel <christopher_dalziel@hotmail.com>
2019-12-05 03:11:48 +1100

    Part 1 complete, working front end for create page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import './App.css';
+import createTodo from "./components/create-todo.component";
+import editTodo from "./components/edit-todo.component";
+import todosList from "./components/todos-list.component";
 
-function App() {
-  return (
-    <div className="container">
-      <h2>MERN-Stack TODO App</h2>
-    </div>
-  );
+import logo from "./logo.png";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        {/* Top Level Div */}
+        <div className="container">
+          {/* Nav bar div, including bootstrap className */}
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://github.com/christopherdalziel" target="_blank">
+              <img src={logo} width="30" height="30" alt="My first MERN project" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack TODO App!</Link>
+            {/* Below line className is collpase on purpose? */}
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          {/* Created Routes */}
+            <Route path="/" exact component={todosList} />
+            <Route path="/edit/:id" component={editTodo} />
+            <Route path="/create" component={createTodo} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class todosList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to TODOS list Component!</p>
+      </div>
+    )
+  }
+}
